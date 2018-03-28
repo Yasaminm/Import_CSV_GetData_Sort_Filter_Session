403cd20fa4eedf845e881332c04d60b7b156b66e
Yasamin Mustamandi <yasamin.mostamandy@gmail.com>
2018-03-28 14:53:36 +0200

    -order -sort ASC/DESC -filter according city input

--- index.php
+++ index.php
@@ -1,31 +1,55 @@
 <?php
+session_start();
+if(!isset($_SESSION['prevOrderCol'])){
+    $_SESSION['prevorderCol'] = ' ';
+}
+
 require_once './config.php';
 
-function getAllData($tblName, $columnNames = ['*'], $orderColumns = ['id']) {
+function getAllData($tblName, $columnNames = ['*'], $orderColumns = ['id'], $sort = 'ASC', $filter="%") {
     $pdo = new PDO('mysql:host=' . HOST . ';dbname=' . DB, USER, PASSWORD);
     $cols = implode(',', $columnNames);
     $orderCols = implode(',', $orderColumns);
-    $query = "SELECT $cols FROM $tblName ORDER BY $orderCols";
+    $where = "WHERE  city LIKE '$filter%'";
+    $query = "SELECT $cols FROM $tblName $where ORDER BY $orderCols $sort";
     $stmt = $pdo->query($query);
     $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
     return $rows;
 }
 
-
-
 $columnNames = ['country', 'province', 'city', 'pop'];
-
+//order
 $orderCol = filter_input(1, 'order', 513);
+if ($orderCol) {
+    $_SESSION['$orderCol'] = $orderCol;
+}elseif(!isset ($_SESSION['orderCol'])){
+    $_SESSION['$orderCol'] = 'country';
+    
+}
 
-$orderCol = ($orderCol) ? $orderCol : 'country';
 
-$rows = getAllData('tb_cities', $columnNames, [$orderCol]);
- 
+$_SESSION[$orderCol] = ($orderCol) ? $orderCol : 'country';
+
+
 //if(isset($orderCol)){ 
 //$rows = getAllData('tb_cities', $columnNames, [$orderCol]);
 //}else{
 //    $rows = getAllData('tb_cities', $columnNames);
 //}
+
+//sort
+$sort = filter_input(1, 'sort', 513);
+if ($_SESSION[$orderCol] !== @$_SESSION['prevOrderCol']){
+    $sort = 'ASC';
+}
+$sort = ($sort === 'ASC') ? 'DESC' : 'ASC';
+$_SESSION['prevorderCol'] = $_SESSION['$orderCol'];
+
+//filter
+$filter = filter_input(1, 'filter', FILTER_SANITIZE_STRING);
+//$filter = ($filter) ? $filter : '%'; 
+$rows = getAllData('tb_cities', $columnNames, [$_SESSION['$orderCol']], $sort, $filter);
+
 ?>
 
 <!DOCTYPE html>
@@ -43,14 +67,20 @@ $rows = getAllData('tb_cities', $columnNames, [$orderCol]);
     </head>
     <body>
         <div class="container">
+            <form cla="form-inline" method="get">
+                <label>
+                    <input placeholder="City" class="form-control" type="text" name="filter" value="<?php echo $filter ?>">
+                </label>
+                <button class="btn btn-primary">Filtern</button>
+            </form>
             <table class="table table-striped">
                 <thead>
                     <tr>
                         
-                        <th><a href="index.php?order=country">Land</a></th>
-                        <th><a href="index.php?order=province">Region</a></th>
-                        <th><a href="index.php?order=city">Stadt</a></th>
-                        <th><a href="index.php?order=pop">Einwohner</a></th>
+                        <th><a href="index.php?order=country&sort=<?php echo $sort ?>">Land</a></th>
+                        <th><a href="index.php?order=province&sort=<?php echo $sort ?>">Region</a></th>
+                        <th><a href="index.php?order=city&sort=<?php echo $sort ?>">Stadt</a></th>
+                        <th><a href="index.php?order=pop&sort=<?php echo $sort ?><?php ?>">Einwohner</a></th>
                     </tr>
                 </thead>
                 <tbody>
